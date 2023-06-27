83f53cd51c5b1f66634fa7ca7093c0bea344b8c4
NehaShukla <nehashukla3309@gmail.com>
2023-06-27 08:45:40 +0530

    modified files

--- routes/all_instructor.js
+++ routes/all_instructor.js
@@ -1,12 +1,14 @@
 const route = require("express").Router();
-const {instructor} = require("../model/instructor_model");
+const { instructor } = require("../model/instructor_model");
 
-route.get("/all_instructor", async(req, res)=>{
-    try {
-        const data = await instructor.find()
-        res.send(data)
-    } catch (error) {
-       console.log(error) 
-    }
-})
-module.exports = route
+route.get("/all_instructor", async (req, res) => {
+  try {
+    const data = await instructor.find();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({
+      message: "Internal server error",
+    });
+  }
+});
+module.exports = route;
